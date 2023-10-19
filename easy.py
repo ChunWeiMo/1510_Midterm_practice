654488f36afea52540662dcfcf5e11468bceb455
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-10-19 16:08:07 -0700

    Complete easy.py

--- easy.py
+++ easy.py
@@ -14,3 +14,40 @@ You must provide a full docstring for this function including all pre- and post-
 You must create two useful and different doctests for this function.
 No main function is required.
 """
+
+
+def count_evens(list1):
+    """
+    Count evens in the input list, and sum those evens.
+
+    :param list1: a non-empty list
+    :precondition: all element in list1 should be integer
+    :postcondition: Count evens in the input list, and sum those evens.
+
+    :return a tuple of even counts and the sum
+    """
+    evens_count = 0
+    sum_evens = 0
+    for number in list1:
+        if number % 2 == 1:
+            evens_count += 1
+            sum_evens += number
+    return (evens_count, sum_evens)
+
+
+def main():
+    list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    tuple1 = count_evens(list1)
+    print(tuple1)
+    
+    list1 = [1,3,3,3,5]
+    tuple1 = count_evens(list1)
+    print(tuple1)
+    
+    list1 = [2,2,2,2,2]
+    tuple1 = count_evens(list1)
+    print(tuple1)
+
+
+if __name__ == "__main__":
+    main()
